8a295767c17e2b1f48585394a0f0a4cede7de345
jonas <jonas@web3.foundation>
2022-02-02 10:35:01 +0100

    attach our_stash to final data

--- 1kv_payout_analysis.R
+++ 1kv_payout_analysis.R
@@ -6,7 +6,7 @@ library(tidyverse)
 # Specify chain: either "polkadot" or "kusama"
 chain = "kusama"
 # Current era - 1 as the data is written at the end of an era
-current_era = 3240
+current_era = 3302
 
 if(chain=="polkadot"){
   normalization = 1/10000000000
@@ -50,12 +50,15 @@ sum_these <- function(df1, to_sum1) {
 
 # Set counter variable
 validators$votes_of_1kv <- 0
+validators$our_stash <- "none"
+
 # Loop to determine if one of the stash addresses is among the stakers, which essentially means that the validator is in 1kv
 for(i in 1:nrow(validators)){
   for(x in 1:length(kusama_1kv_stash)){
     find <- grepl(kusama_1kv$kusama_1kv_stash[x], validators$stakers[i])
     if(find==TRUE){
       validators$votes_of_1kv[i] <- validators$votes_of_1kv[i] + 1
+      validators$our_stash[i] <- kusama_1kv_stash[x]
     }
   }
 }
@@ -65,7 +68,7 @@ validators_use <- subset(validators, votes_of_1kv>=1)
 
 # Generate dataset which extracts the amount of our stake with the validator. In the case that more 1kv-stashes stake with a validator, we take the sum (this might never be the case)
 df <- tibble(
-  validators_use$name, validators_use$commission_percent, validators_use$self_stake, validators_use$total_stake, validators_use$era_points, 
+  validators_use$name, validators_use$commission_percent, validators_use$self_stake, validators_use$total_stake, validators_use$era_points, validators_use$our_stash,
   val = decode(validators_use$stakers), 
   to_sum = list(kusama_1kv_stash), 
   our_stake = map2_dbl(val, to_sum, sum_these)
