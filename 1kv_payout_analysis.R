b595c3a3c18db5d6cc0d99e486e706e937d4e215
jonas <jonas@web3.foundation>
2022-02-03 13:29:03 +0100

    added polkadot 1kv-stashes

--- 1kv_payout_analysis.R
+++ 1kv_payout_analysis.R
@@ -8,16 +8,21 @@ chain = "kusama"
 # Current era - 1 as the data is written at the end of an era
 current_era = 3302
 
+
+
 if(chain=="polkadot"){
   normalization = 1/10000000000
+  stash_1kv <- c('14Ns6kKbCoka3MS4Hn6b7oRw9fFejG8RH5rq5j63cWUfpPDJ', '16GMHo9HZv8CcJy4WLoMaU9qusgzx2wxKDLbXStEBvt5274B', '12RYJb5gG4hfoWPK3owEYtmWoko8G6zwYpvDYTyXFVSfJr8Y')
+  names_1kv <- c("1kv-stash-1", "1kv-stash-2", "1kv-stash-3")
 }else{
   normalization = 1/1000000000000
+  stash_1kv <- c('EX9uchmfeSqKTM7cMMg8DkH49XV8i4R7a7rqCn8btpZBHDP', 'G1rrUNQSk7CjjEmLSGcpNu72tVtyzbWdUvgmSer9eBitXWf', 'HgTtJusFEn2gmMmB5wmJDnMRXKD6dzqCpNR7a99kkQ7BNvX')
+  names_1kv <- c("1kv-stash-1", "1kv-stash-2", "1kv-stash-3")
 }
 
 # Define the 1kv Stashes
-kusama_1kv_stash <- c('EX9uchmfeSqKTM7cMMg8DkH49XV8i4R7a7rqCn8btpZBHDP', 'G1rrUNQSk7CjjEmLSGcpNu72tVtyzbWdUvgmSer9eBitXWf', 'HgTtJusFEn2gmMmB5wmJDnMRXKD6dzqCpNR7a99kkQ7BNvX')
-kusama_1kv_names <- c("1kv-stash-1", "1kv-stash-2", "1kv-stash-3")
-kusama_1kv <- data.frame(kusama_1kv_stash, kusama_1kv_names)
+
+info_1kv <- data.frame(stash_1kv, names_1kv)
 
 # Import Data for T
 validators <- read.csv(url(paste("https://storage.googleapis.com/watcher-csv-exporter/", chain , "_validators_era_", current_era, ".csv", sep=(""))),stringsAsFactors = FALSE)
@@ -59,11 +64,11 @@ validators$our_stash <- "none"
 
 # Loop to determine if one of the stash addresses is among the stakers, which essentially means that the validator is in 1kv
 for(i in 1:nrow(validators)){
-  for(x in 1:length(kusama_1kv_stash)){
-    find <- grepl(kusama_1kv$kusama_1kv_stash[x], validators$stakers[i])
+  for(x in 1:length(stash_1kv)){
+    find <- grepl(info_1kv$stash_1kv[x], validators$stakers[i])
     if(find==TRUE){
       validators$votes_of_1kv[i] <- validators$votes_of_1kv[i] + 1
-      validators$our_stash[i] <- kusama_1kv_stash[x]
+      validators$our_stash[i] <- stash_1kv[x]
     }
   }
 }
@@ -75,7 +80,7 @@ validators_use <- subset(validators, votes_of_1kv>=1)
 df <- tibble(
   validators_use$name, validators_use$commission_percent, validators_use$self_stake, validators_use$total_stake, validators_use$era_points, validators_use$our_stash,
   val = decode(validators_use$stakers), 
-  to_sum = list(kusama_1kv_stash), 
+  to_sum = list(stash_1kv), 
   our_stake = map2_dbl(val, to_sum, sum_these)*normalization
 )
 
