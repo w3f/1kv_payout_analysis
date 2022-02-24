cfae2d6319329e4dc6a52d555c1230e3e7868955
jonas <jonas@web3.foundation>
2022-02-24 09:53:18 +0100

    added loop to get data from multiple eras and summarize later

--- 1kv_payout_analysis.R
+++ 1kv_payout_analysis.R
@@ -8,6 +8,7 @@ chain = "kusama"
 # Current era - 1 as the data is written at the end of an era
 current_era = 3366
 
+first_era = 3363
 
 
 if(chain=="polkadot"){
@@ -24,78 +25,90 @@ if(chain=="polkadot"){
 
 info_1kv <- data.frame(stash_1kv, names_1kv)
 
-# Import Data for T
-validators <- read.csv(url(paste("https://storage.googleapis.com/watcher-csv-exporter/", chain , "_validators_era_", current_era, ".csv", sep=(""))),stringsAsFactors = FALSE)
-
-# Import Data from T+1 and only extract the rewards paid to validators.
-validators_next <- read.csv(url(paste("https://storage.googleapis.com/watcher-csv-exporter/", chain , "_validators_era_", current_era + 1, ".csv", sep=(""))),stringsAsFactors = FALSE)
-validator_rewards <- validators_next$validator_rewards_previous_era[1]
-
-# Normalize values to DOT / KSM
-validators$self_stake <- validators$self_stake*normalization
-validators$total_stake <- validators$total_stake*normalization
-validator_rewards <- validator_rewards*normalization
-
-# Only take active validators
-validators <- subset(validators, active==1)
-
-# Very rare bug where the "stakers" variable is empty - should investigate this later.
-validators <- subset(validators, stakers != "")
-
-decode <- function(code) {
-  # return a list of data frames for the decoded string vector code
-  code %>% 
-    str_split(",") %>% 
-    map(str_split, ";", simplify = TRUE) %>% 
-    map(as.data.frame) %>% 
-    map(set_names, c("name", "value")) %>%
-    map(~mutate_all(.,as.character)) %>%
-    map(mutate, value = as.numeric(value))
-}
-
-sum_these <- function(df1, to_sum1) {
-  # for one data frame of name, value (df1), sum the values for name in to_sum1
-  df1 %>% filter(name %in% to_sum1) %>% pull(value) %>% as.numeric() %>% sum()
-}
-
-# Set counter variable
-validators$votes_of_1kv <- 0
-validators$our_stash <- "none"
-validators$our_stash_name <- "none"
-
-# Loop to determine if one of the stash addresses is among the stakers, which essentially means that the validator is in 1kv
-for(i in 1:nrow(validators)){
-  for(x in 1:length(stash_1kv)){
-    find <- grepl(info_1kv$stash_1kv[x], validators$stakers[i])
-    if(find==TRUE){
-      validators$votes_of_1kv[i] <- validators$votes_of_1kv[i] + 1
-      validators$our_stash[i] <- stash_1kv[x]
-      validators$our_stash_name[i] <- names_1kv[x]
+difference = current_era - first_era
+x = c(current_era:(current_era - difference))
+# Loop through Era data at the end point. If one era is missing, it uses the one from before to fill the gap. That means, at least the first era needs to be non-missing.
+for(i in 1:length(x)) {
+  tryCatch(
+    validators <- read.csv(url(paste("https://storage.googleapis.com/watcher-csv-exporter/", chain , "_validators_era_", current_era, ".csv", sep=(""))),stringsAsFactors = FALSE),
+    validators_next <- read.csv(url(paste("https://storage.googleapis.com/watcher-csv-exporter/", chain , "_validators_era_", current_era + 1, ".csv", sep=(""))),stringsAsFactors = FALSE),
+    error = function(e){
+      print("There was an Error")
+      missing_validator <<- missing_validator + 1
+      validators$era <<- validators$era + 1
+    })
+  validator_rewards <- validators_next$validator_rewards_previous_era[1]
+  # Normalize values to DOT / KSM
+  validators$self_stake <- validators$self_stake*normalization
+  validators$total_stake <- validators$total_stake*normalization
+  validator_rewards <- validator_rewards*normalization
+  
+  # Only take active validators
+  validators <- subset(validators, active==1)
+  
+  # Very rare bug where the "stakers" variable is empty - should investigate this later.
+  validators <- subset(validators, stakers != "")
+  
+  # Set counter variable
+  validators$votes_of_1kv <- 0
+  validators$our_stash <- "none"
+  validators$our_stash_name <- "none"
+  
+  # Loop to determine if one of the stash addresses is among the stakers, which essentially means that the validator is in 1kv
+  for(t in 1:nrow(validators)){
+    for(x in 1:length(stash_1kv)){
+      find <- grepl(info_1kv$stash_1kv[x], validators$stakers[t])
+      if(find==TRUE){
+        validators$votes_of_1kv[t] <- validators$votes_of_1kv[t] + 1
+        validators$our_stash[t] <- stash_1kv[x]
+        validators$our_stash_name[t] <- names_1kv[x]
+      }
     }
   }
+  validators_use <- subset(validators, votes_of_1kv>=1)
+  
+  # Generate dataset which extracts the amount of our stake with the validator. In the case that more 1kv-stashes stake with a validator, we take the sum (this might never be the case)
+  df <- tibble(
+    validators_use$stash_address, validators_use$name, validators_use$commission_percent, validators_use$self_stake, validators_use$total_stake, validators_use$era_points, validators_use$our_stash, validators_use$our_stash_name,
+    val = decode(validators_use$stakers), 
+    to_sum = list(stash_1kv), 
+    our_stake = map2_dbl(val, to_sum, sum_these)*normalization
+  )
+  # Create final data table
+  df_output <- subset(df, select=c(`validators_use$stash_address`, `validators_use$name`,`validators_use$commission_percent`,`validators_use$total_stake`, `validators_use$self_stake`,`validators_use$era_points`,`validators_use$our_stash`, our_stake,`validators_use$our_stash_name`))
+  colnames(df_output) <- c('stash_address', 'name', 'commission', 'total_stake', 'self_stake', 'era_points', 'our_stash', 'our_stake', 'our_stash_name')
+  
+  # Calculate our payoff (generated by our stashes) per validator
+  df_output$our_payoff <- (df_output$era_points / sum(subset(validators, active==1)$era_points)) * validator_rewards * ((100-df_output$commission)/100) * (df_output$our_stake / df_output$total_stake)
+  # Calculate overall payoff per validator
+  df_output$validator_payoff <- (df_output$era_points / sum(subset(validators, active==1)$era_points)) * validator_rewards
+  df_aggregate <- aggregate(df_output$our_payoff, by=list(df_output$our_stash), FUN=sum)
+  
+  if(i==1){
+    df_total_output <- df_output
+    df_total_aggregate <- df_aggregate
+  } else {
+    df_total_output <- rbind(df_total_output, df_output)
+    df_total_aggregate <- rbind(df_total_aggregate, df_aggregate)
+  }
+  decode <- function(code) {
+    # return a list of data frames for the decoded string vector code
+    code %>% 
+      str_split(",") %>% 
+      map(str_split, ";", simplify = TRUE) %>% 
+      map(as.data.frame) %>% 
+      map(set_names, c("name", "value")) %>%
+      map(~mutate_all(.,as.character)) %>%
+      map(mutate, value = as.numeric(value))
+  }
+  
+  sum_these <- function(df1, to_sum1) {
+    # for one data frame of name, value (df1), sum the values for name in to_sum1
+    df1 %>% filter(name %in% to_sum1) %>% pull(value) %>% as.numeric() %>% sum()
+  }
 }
+df_final <- aggregate(df_total_output$our_payoff, by=list(df_total_output$stash_address), FUN=sum)
 
-# Take subset of only 1kv validators
-validators_use <- subset(validators, votes_of_1kv>=1)
 
-# Generate dataset which extracts the amount of our stake with the validator. In the case that more 1kv-stashes stake with a validator, we take the sum (this might never be the case)
-df <- tibble(
-  validators_use$stash_address, validators_use$name, validators_use$commission_percent, validators_use$self_stake, validators_use$total_stake, validators_use$era_points, validators_use$our_stash, validators_use$our_stash_name,
-  val = decode(validators_use$stakers), 
-  to_sum = list(stash_1kv), 
-  our_stake = map2_dbl(val, to_sum, sum_these)*normalization
-)
-
-# Create final data table
-df_output <- subset(df, select=c(`validators_use$stash_address`, `validators_use$name`,`validators_use$commission_percent`,`validators_use$total_stake`, `validators_use$self_stake`,`validators_use$era_points`,`validators_use$our_stash`, our_stake,`validators_use$our_stash_name`))
-colnames(df_output) <- c('stash_address', 'name', 'commission', 'total_stake', 'self_stake', 'era_points', 'our_stash', 'our_stake', 'our_stash_name')
-
-# Calculate our payoff (generated by our stashes) per validator
-df_output$our_payoff <- (df_output$era_points / sum(subset(validators, active==1)$era_points)) * validator_rewards * ((100-df_output$commission)/100) * (df_output$our_stake / df_output$total_stake)
-# Calculate overall payoff per validator
-df_output$validator_payoff <- (df_output$era_points / sum(subset(validators, active==1)$era_points)) * validator_rewards
-write.csv(df_output, 'output.csv')
-
-df_aggregate <- aggregate(df_output$our_payoff, by=list(df_output$our_stash), FUN=sum)
 
 
